a7f71f3608bd9a930d5c522229bf27084dd9d8fa
kmkan <kandpalkbusiness@gmail.com>
2024-12-21 11:24:28 -0700

    proj1a: Created constructor for LinkedListDeque61B

--- proj1a/src/deque/LinkedListDeque61B.java
+++ proj1a/src/deque/LinkedListDeque61B.java
@@ -1,4 +1,69 @@
 package deque;
 
-public class LinkedListDeque61B {
+import java.util.List;
+
+public class LinkedListDeque61B<T> implements Deque61B<T> {
+
+    public class Node {
+        Node prev;
+        T item;
+        Node next;
+
+        public Node(Node p, T i, Node n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    Node sentinel;
+
+    public LinkedListDeque61B() {
+        sentinel = new Node(null, null, null);
+    }
+
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
 }
